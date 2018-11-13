ef0826576f279760ed32b634066ed843d583383a
Barikansari <ansari.barik3@gmail.com>
2018-11-13 07:54:11 +0545

    file read

---
+++ HealthData-Analysing-System/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HealthData_Analysing_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                string file = openFileDialog1.FileName;
+                string text = System.IO.File.ReadAllText(file);
+
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
